37a33ff7af22b2c9293a5cb4011610f72142ebe4
maharadgy-242 <116069313+maharadgy-242@users.noreply.github.com>
2022-10-24 22:39:18 +0200

    improve apiUrl

--- src/app.js
+++ src/app.js
@@ -41,6 +41,7 @@ function displayTemperatire(response) {
 }
 
 let apiKey = "6db09e02ab36d020dda52839a3960ba8";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+let city = "Kyiv";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperatire);
