738ab810c1b38429a76c71a34efb1ceb2718004c
maharadgy-242 <116069313+maharadgy-242@users.noreply.github.com>
2022-10-24 13:45:33 +0200

    Added js for temperature, city and description

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+function displayTemperatire(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
 
+let apiKey = "6db09e02ab36d020dda52839a3960ba8";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperatire);
